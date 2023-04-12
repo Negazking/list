83dcf69dc0d3cad3addbcdd0c806bc24455ee406
Negazking <discordakun34141@gmail.com>
2023-04-12 11:30:40 +0700

    a

--- src/components/ListData.js
+++ src/components/ListData.js
@@ -1,34 +1,8 @@
 import React from "react";
 import ItemData from "./ItemData";
+import DataHewan from "../utils/data";
 function ListData(){
-    const DataHewan=[
-        {
-          
-          itemImages: '/images/kudabro.jpeg',
-          ItemNama:'Kuda',
-          Itemdesc:'Para ilmuwan awalnya berpendapat bahwa leluhur kuda modern pertama kali didomestikasi sekitar tahun 3500 SM atau 5500 tahun yang lalu di Botai, Asia Tengah. '
-        },
-
-        {
-          
-          itemImages: '/images/kucing.jpeg',
-          ItemNama:'kucing',
-          Itemdesc:'Hewan ini merupakan binatang yang diyakini sudah ada sejak 6.000 tahun SM dengan beberapa bukti berupa kerangka yang ditemukan di Pulau Siprus. '
-        },
-        {
-          
-          itemImages: '/images/orangUtan.jpeg',
-          ItemNama:'Orang Utan',
-          Itemdesc:'Orang utan (bentuk tidak baku: orangutan) atau mawas adalah kera besar yang berasal dari hutan hujan Indonesia dan Malaysia. Sekarang mereka hanya ditemukan di sebagian Kalimantan dan Sumatra,'
-        },
-        {
-          
-          itemImages: '/images/Capybara.jpg',
-          ItemNama:'Capybara',
-          Itemdesc:'Kapibara[a] atau kapibara besar (Hydrochoerus hydrochaeris) merupakan jenis hewan pengerat terbesar yang masih ada di dunia (hewan pengerat terbesar yang telah punah adalah Phoberomys pattersoni)[1] yang merupakan anggota genus Hydrochoerus. '
-        },
-        
-    ]
+    
     return (
         <div>
             {
